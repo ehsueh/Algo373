fed7f483e9209c50910caf5383f3732772198dc9
ehsueh <ehsueh1993@gmail.com>
2014-04-04 21:41:28 -0400

    fixed some bugs

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 Algo373
 =======
 
-Algo373
+Algo373 Coding Project
+
+Grace Lin 
+Emma Hsueh 998968951 
+
+Q1 hop
+Q2 monkey
+are done in python
+
+Q3 boxes
+Q4 business (our approximation attempt)
+are done in c++
+
+To compile please use the c++ -fpermissive flag.
+Thanks!
--- boxes/boxes.cc
+++ boxes/boxes.cc
@@ -4,23 +4,31 @@
 #include <vector>
 #include <stdlib.h>
 #include <algorithm>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
+int iterations=0;
 //this is really similar to calculating 
 //the levenshtein distance
 //using the Wagner-Fischer algo
 int LevDist(string s, string t, int len_s, int len_t) { 
 
 //initializing matrix 
-int D[len_s][len_t];
+//int D[len_s][len_t];
+
+// int (*D)[len_t] = malloc(sizeof *D * len_s);
+int *D = malloc( sizeof(int) * (len_s + 1) * (len_t + 1));
+memset(D, '\0', sizeof(D));
+
 
 //distance to empty string 
 for (int i = 0;i<len_s;i++)
-	D[i][0] = i; 
+	D[i * len_t + 0] = i; 
 
 for (int j = 0;j<len_t;j++)
-	D[0][j] = j; 
+	D[0 * len_t + j] = j; 
 
 //use dynamic programming
 //DP[i,j] = the min number of operations (modified edit distance) 
@@ -28,47 +36,67 @@ for (int j = 0;j<len_t;j++)
 //to substack(t, 0, j)
 for (int i=1; i<len_s+1; i++) 
 	for(int j=1; j<len_t+1; j++) { 
+		iterations++;
 		// uncomment to show steps
-		// cout << "s sub: " << s.substr(0,i) << endl << "t sub: "<< t.substr(0,j) << endl;
-		if (s.substr(i-1,1) == t.substr(j-1,1)) { //string indexing starts at zero
-			D[i][j] = D[i-1][j-1];
+		cout << "s sub: " << s.substr(0,i) << endl << "t sub: "<< t.substr(0,j) << endl;
+		if (s.substr(i-1,1) == t.substr(j-1,1)) {//string index starts at zero. D[0,0] for empty strings
+			D[i * len_t + j] = D[(i-1) * len_t + j-1];
 			// uncomment to show steps
-			// cout << "i,j = " << i << "," << j << endl;
-			// cout << s.substr(i,1) << "==" << t.substr(j,1) << endl << D[i][j] << endl;
+			cout << "i,j = " << i << "," << j << endl;
+			cout << s.substr(i-1,1) << "==" << t.substr(j-1,1) << endl << "D[i][j]: " << D[i * len_t + j] << endl;
+			cout << "--------" << iterations << endl;
 		} else { 
-			if (i==len_s) {
-				D[i][j] = min(
+			if (i <= j) {
+				if (i == len_s){
+				D[i * len_t + j] = min(
+					//then we have the option of 
 					//adding a box at the top of s
-				 	D[i][j-1]+1, 
-				 	min(
+				 	D[i * len_t + j-1] + 1, 
+				 	// min(
 				 		//deleting this box from s
-				 		D[i-1][j]+1,
+				 		// D[(i-1) * len_t + j ]+ 1,
 				 		//substitute a box
-						D[i-1][j-1] + 1)); 
-				// uncomment to show steps 
-				// cout << "D[i][j]: " << D[i][j] << endl;
+						D[(i-1) * len_t + j-1] + 1);
+				} else {
+					D[i * len_t + j] = D[(i-1) * len_t + j-1] + 1;
+				} 
 			} else {
-				D[i][j] = min(
+				if (i == len_s){
+					D[i * len_t + j] = min( 
+					D[i * len_t + j-1] + 1, min(
+					D[(i-1) * len_t + j] + 1,
+					D[(i-1) * len_t + j-1] + 1));
+				} else {
+					D[i * len_t + j] = min(
 					 //deleting a box anywhere from s
-					 D[i-1][j]+1, 
+					 D[(i-1) * len_t + j] + 1, 
 					 //substitute a box
-					 D[i-1][j-1] + 1);
-				// uncomment to show steps 
-				// cout << "D[i][j]: " << D[i][j] << endl;
+					 D[(i-1) * len_t + j-1] + 1);
+				}
 			}
+				// uncomment to show steps 
+cout << "D[i][j]: " << D[i * len_t + j] << endl;
+cout << "--------" << iterations << endl;
+
 		}
 	};
-//return the min operation to get from s to t 
-return D[len_s][len_t];
+
+	free(D);
+	int d =D[len_s * len_t + len_t]; 
+//return the min operation to get from s to t   
+return d;
 }
 
 
 int main(int argc, char ** argv)
 {
 	if (argc == 2) {
+		clock_t start, stop;
+		start = clock();
+
 		std::ifstream file(argv[1]);
 		int sum = 0;
-		int rno, cno, lineno=0;
+		int levDist, rno, cno, lineno=0;
 		//initialize a vector of strings
 		//strings[0] to strings[cno-1] are "s" strings
 		//strings[cno] to strings[2cno-1] are the corresponding "t" strings
@@ -84,7 +112,6 @@ int main(int argc, char ** argv)
 							strings.push_back("");
 				} else {//if no evil test cases
 						//rno and cno should be nonzero
-
 						//append this char to start of string if it's not "-"		
 						for(int c=0; c<cno; c++)
 							if (line.substr(2*c,1) != "-")
@@ -98,14 +125,18 @@ int main(int argc, char ** argv)
 			std::string s = strings[i];
 			std::string t = strings[i + cno];
 			// uncomment below to see steps
-			// cout << "string s: " << s << endl;
-			// cout << "string t: " << t << endl;
-			sum += LevDist(s, t, s.size(), t.size());
+			cout << "string s: " << s << endl;
+			cout << "string t: " << t << endl;
+			levDist = LevDist(s, t, s.size(), t.size());
+			sum += levDist;
 			// uncomment below to see steps
-			// cout << "LevDist: " << LevDist(s, t, s.size(), t.size()) << endl << endl;
+			cout << "LevDist: " << levDist << endl << endl;
 		}
 
 		cout << sum << endl;
+		stop = clock();
+		double timeTaken = (double) (stop - start)/ CLOCKS_PER_SEC;
+		cout << "Time taken: " << timeTaken << "sec" << endl;
 
 	} else {
 		cout << "usage: " << argv[0] << " <input file>" << endl;
